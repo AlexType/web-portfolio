577edc77579cdf4e9b8e84705d401a1a89281010
Alexandr Bryzgalov <arurulras@icloud.com>
2021-05-01 00:08:19 +1000

    upd: pages <Home>

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,33 @@
-import React, { Fragment } from 'react';
+import React from 'react';
+import TimeLine from '../../components/TimeLine/TimeLine';
 
 const Home = () => {
     return (
-        <Fragment>
-            <div className="container">
-                <p>Home</p>
+        <article className='intro'>
+            <div className="container py-5">
+                <div className="row justify-content-between">
+                    <div className="col-7">
+                        <h2 className="title title_name">
+                            <span className="text-red">#</span>
+                            Alexandr Bryzgalov
+                        </h2>
+                        <p className='fs-2 fw-bold text-end mt-3'>
+                            <span className="text-red">&lt;</span>
+                            Front-End Developer
+                            <span className="text-red"> /&gt;</span>
+                        </p>
+                    </div>
+                    <div className="col-auto">
+                        <h3 className="title mb-4 text-end">
+                            <span className="text-red">[...</span>
+                            TimeLine
+                            <span className="text-red">]</span>
+                        </h3>
+                        <TimeLine />
+                    </div>
+                </div>
             </div>
-        </Fragment>
+        </article>
     );
 };
 
